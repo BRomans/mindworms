a8e4f0c25b53afaec2005d237fafd414d38791e5
BRomans <michele.romani.gzl0@gmail.com>
2021-05-09 11:52:55 +0200

    :sparkles: implemented simple character controller

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float movementSpeed = 1;
+    public float jumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+   
+        if(Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
